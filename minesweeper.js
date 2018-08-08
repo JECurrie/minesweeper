d68767fabde21cc774527d0e149001e3be7544e7
joancurrie <jecurrie8@gmail.com>
2018-08-08 15:04:12 -0400

    Update minesweeper.js

--- minesweeper.js
+++ minesweeper.js
@@ -5,21 +5,16 @@ const board = [
   [' ',' ',' ']
 ];
 
-
-console.log('Current board:');
 const printBoard = board => {
   // Log 'Current Board: '
   // Log the first element of the board variable
+  console.log('Current board:');
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
 };
-console.log(board[0].join(' | '));
-console.log(board[1].join(' | '));
-console.log(board[2].join(' | '));
-console.log(printBoard(board));
 
-console.log('Current board:');
+console.log(printBoard(board));
 board[0][1] = '1';
 board[2][2] = 'B';
-console.log(board[0].join(' | '));
-console.log(board[1].join(' | '));
-console.log(board[2].join(' | '));
 console.log(printBoard(board));
